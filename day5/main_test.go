2c8940421e05451a69d90415a5a4aa24e6b67e3a
Abdessamad ANSSEM <abdoanss@proton.me>
2024-12-05 00:41:26 +0100

    day5 prep!

---
+++ day5/main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestLoadInput(t *testing.T) {
+	expected := []string{
+		"testinput",
+	}
+
+	result := LoadInput("input_test.txt")
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("expected %v but got %v", expected, result)
+	}
+}
+
+func TestPartOne(t *testing.T) {
+	expected := 18
+	result := part1("input_test.txt")
+
+	if result != expected {
+		t.Errorf("expected %d, got %d", expected, result)
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	expected := 9
+	result := part2("input_test.txt")
+
+	if result != expected {
+		t.Errorf("expected %d, got %d", expected, result)
+	}
+}
